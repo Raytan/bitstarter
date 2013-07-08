dda98381aa114e6680342ce083ef084727217e09
Raytan <twlsmail@gmail.com>
2013-07-08 03:01:52 +0000

    Reade from index.html

--- web.js
+++ web.js
@@ -3,7 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var inputfile = "index.html";
+  var buffer = new Buffer (fs.readFileSync(inputfile), "utf-8");
+  var str = buffer.toString();
+
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
